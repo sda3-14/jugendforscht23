622d92fde0b67c7604ff3d222868e482e1a307bc
Jacob Frodermann <j.frodermann@gmx.net>
2023-01-14 21:16:44 +0100

    yeet

--- src/main/java/jugendforscht23/client/Benchmark.java
+++ src/main/java/jugendforscht23/client/Benchmark.java
@@ -1,7 +1,9 @@
 package jugendforscht23.client;
 
-import io.netty.buffer.ByteBufUtil;
-import io.netty.buffer.Unpooled;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelPipeline;
@@ -11,15 +13,38 @@ import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 
 public class Benchmark {
+	private static final Logger LOG = LoggerFactory.getLogger(Benchmark.class);
+
 	public static void init(ChannelPipeline pipeline) {
 		pipeline.addLast(
 			new ProtobufVarint32FrameDecoder(),
 			new ProtobufVarint32LengthFieldPrepender(),
-			new LoggingHandler(LogLevel.INFO),
+			new LoggingHandler(LogLevel.DEBUG),
 			new ChannelInboundHandlerAdapter() {
 				public void channelActive(ChannelHandlerContext ctx) throws Exception {
 					super.channelActive(ctx);
-					ctx.writeAndFlush(Unpooled.wrappedBuffer(ByteBufUtil.decodeHexDump("1234567890")));
+					new Thread(new Runnable() {
+						public void run() {
+							try {
+								while(true) {
+									long t = System.nanoTime();
+									ByteBuf b = ctx.alloc().buffer();
+									b.writeLong(t);
+									ctx.writeAndFlush(b);
+									Thread.sleep(10);
+								}
+							} catch (InterruptedException e) {
+								e.printStackTrace();
+							}
+						}
+					}).start();
+				}
+				public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+					assert msg instanceof ByteBuf;
+					ByteBuf buf = (ByteBuf) msg;
+					long t = buf.readLong();
+					t = System.nanoTime() - t;
+					LOG.info("delay:" + t/1000000d);
 				}
 			}
 		);
--- src/main/java/jugendforscht23/server/PingHandler.java
+++ src/main/java/jugendforscht23/server/PingHandler.java
@@ -6,15 +6,12 @@ import io.netty.channel.ChannelPipeline;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.protobuf.ProtobufVarint32FrameDecoder;
 import io.netty.handler.codec.protobuf.ProtobufVarint32LengthFieldPrepender;
-import io.netty.handler.logging.LogLevel;
-import io.netty.handler.logging.LoggingHandler;
 
 public class PingHandler {
 	public static void init(ChannelPipeline pipeline) {
 		pipeline.addLast(
 			new ProtobufVarint32FrameDecoder(),
 			new ProtobufVarint32LengthFieldPrepender(),
-			new LoggingHandler(LogLevel.INFO),
 			new SimpleChannelInboundHandler<ByteBuf>() {
 				protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) throws Exception {
 					ctx.writeAndFlush(msg.retain());
